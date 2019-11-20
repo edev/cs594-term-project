e6926337ee13e3b9000a14327307d2a32e115e62
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-11-20 15:22:47 -0800

    Reviewed server code.

--- lib/server.rb
+++ lib/server.rb
@@ -5,26 +5,39 @@ require 'socket'
 CLI_PROMPT_TEXT = "server> "
 
 module Chat
-    class ConnectedClient
-        attr_accessor :socket, :thread, :display_name
-
-        def receive
-            @socket.receive
-        end
-
-        def send(*args)
-            @socket.send(*args)
-        end
-    end
-
     ##
-    # Provides all the necessary facilities for host an IRC chat server per the project RFC.
+    # Provides all the necessary facilities for hosting an IRC chat server per the project RFC.
     #
-    # A server quickstart is available via the #start method using default options. However, it's also possible
+    # A server quickstart is available via the start method using default options. However, it's also possible
     # to fully customize the launch sequence and the various components by either inheriting from this class
     # or by simply calling the public methods in this class in a different manner. To create a custom launch sequence,
-    # use the #start method's source as a starting point.
+    # use the start method's source as a starting point.
     class Server
+        ##
+        # Encapsulates client-specific information that the server needs to store and provides convenience methods
+        # to send and receive data (also accessible via Client#socket#send and #Client#socket#receive).
+        class Client
+            attr_accessor :socket, :thread, :display_name
+
+            def initialize(socket)
+                @socket = socket
+
+                # Modify socket's singleton class to include the Sendable and Receivable modules.
+                class << @socket
+                    include Sendable
+                    include Receivable
+                end
+            end
+
+            def receive
+                @socket.receive
+            end
+
+            def send(*args)
+                @socket.send(*args)
+            end
+        end
+
         ##
         # port - the port the server should listen on.
         def initialize(port)
@@ -33,18 +46,34 @@ module Chat
 
             # The fields in this block are all synchronized by the lock below.
             @client_info_lock = Mutex.new
-            @clients = {}         # Keys are display names, values are ConnectedClient objects.
-            @rooms = {}           # Keys are room names, values are lists of users (ConnectedClient) in each room.
+            @clients = {}         # Keys are display names, values are Client objects.
+            @rooms = {}           # Keys are room names, values are lists of users (Client) in each room.
         end
 
-        private def create_or_join_room(room_name, client)
+        ##
+        # Creates or adds the given client to the given room.
+        #
+        # If name is valid, returns true.
+        #
+        # If name is invalid (e.g. contains spaces or is blank), returns (false, reason) where
+        # reason is a text explanation suitable to send to the client, e.g. in an Error message.
+        private def create_or_join_room(name, client)
+            if name.nil?
+                return false, "Room name cannot be nil."
+            elsif name.strip == ""
+                return false, "Room name cannot be blank."
+            elsif /\s/ =~ name
+                return false, "Room name cannot contain whitespace."
+            end
+
             @client_info_lock.synchronize do
-                if @rooms.has_key? room_name
-                    @rooms[room_name] << client unless @rooms[room_name].include? client
+                if @rooms.has_key? name
+                    @rooms[name] << client unless @rooms[name].include? client
                 else
-                    @rooms[room_name] = [client]
+                    @rooms[name] = [client]
                 end
             end
+            true
         end
 
         private def each_socket(&block)
@@ -57,7 +86,7 @@ module Chat
         end
 
         ##
-        # Removes client from the named room. If client was the member of the room, deletes the room.
+        # Removes client from the named room. If client was the last member of the room, deletes the room.
         #
         # If the room was found and the client was in the room, returns true.
         #
@@ -82,10 +111,23 @@ module Chat
             true
         end
 
+        ##
+        # Attempts to register a new name on behalf of a client by making a new name => client mapping in @clients.
+        #
+        # Returns true if the mapping was successful or (false, reason) if the mapping could not be made, where
+        # reason is a text explanation suitable to send to the client, e.g. in an Error message.
         private def register_name(name, client)
+            if name.nil?
+                return false, "Name cannot be nil."
+            elsif name.strip == ""
+                return false, "Name cannot be blank."
+            elsif /\s/ =~ name
+                return false, "Name cannot contain whitespace."
+            end
+
             @client_info_lock.synchronize do
                 if @clients.has_key? name
-                    return false
+                    return false, "That name is already taken."
                 end
                 @clients[name] = client
                 return true
@@ -96,10 +138,9 @@ module Chat
         # Looks up the members of the named room and returns a reply for the requesting client.
         private def room_members(name)
             if name.nil?
-                # TODO send an Error message.
+                # TODO build an Error message.
                 STDERR.puts "Room name cannot be nil."
-            end
-            if name == ""
+            elsif name == ""
                 # Send client list.
                 members = @client_info_lock.synchronize { @clients.keys.dup }
                 RoomMemberList.build name, members
@@ -108,7 +149,7 @@ module Chat
                 members = @client_info_lock.synchronize { @rooms[name].map &:display_name }
                 RoomMemberList.build name, members
             else
-                # TODO Send Error message.
+                # TODO build an Error message.
                 STDERR.puts "Room #{name} does not exist."
             end
         end
@@ -126,6 +167,8 @@ module Chat
                         @rooms.delete room_name
                     end
                 end
+
+                # Remove the client from @clients, which also frees up the client's display_name.
                 @clients.delete client.display_name
             end
         end
@@ -133,17 +176,8 @@ module Chat
         ##
         # Initializes an incoming client connection and handles the greeting sequence.
         #
-        # client - a ConnectedClient.
+        # client - a Client object.
         def greet(client)
-            # Retrieve the current thread to use the thread-local data store for client-specific state.
-            thread = Thread.current
-
-            # Modify socket's singleton class to include the Sendable and Receivable modules.
-            class << client.socket
-                include Sendable
-                include Receivable
-            end
-
             loop do
                 begin
                     message = client.receive
@@ -164,22 +198,21 @@ module Chat
                         return false
                     end
 
-                    if !(/^\S+$/ === message[:displayName])
+                    unless /^\S+$/ =~ message[:displayName]
                         # Fatal: invalid displayName.
                         client.send DeclineGreeting.build("Invalid display name. Spaces are not allowed.")
                         return false
                     end
 
-                    if register_name(message[:displayName], client) == false
-                        # Fatal: username already taken.
-                        client.send DeclineGreeting.build("Display name is already in use. Please choose a different name.")
+                    registered, reason = register_name(message[:displayName], client)
+                    if registered == false
+                        client.send DeclineGreeting.build(reason)
                         return false
                     end
 
-                    # This is the first greeting, and everything checks out.
+                    # Everything checks out.
+                    @client_info_lock.synchronize { client.display_name = message[:displayName] }
                     client.send AcceptGreeting.build
-                    thread[:greeting_done] = true
-                    client.display_name = message[:displayName]
                     return true
                 end
             end
@@ -188,7 +221,7 @@ module Chat
         ##
         # Listens to input on a socket and responds.
         #
-        # client - a ConnectedClient.
+        # client - a Client.
         def listen(client)
             loop do
                 begin
@@ -204,15 +237,22 @@ module Chat
                 when :SKIP
                     next
                 when JoinRoom
-                    # TODO Check whether the room name is valid (no spaces). Otherwise, send error response.
-                    create_or_join_room message[:name], client
+                    created, reason = create_or_join_room message[:name], client
+                    if created == false
+                        # TODO Send error message.
+                        STDERR.puts reason
+                    end
                 when RequestRoomList
-                    client.send RoomList.build(@rooms.keys)
+                    room_list = @client_info_lock.synchronize { @rooms.keys.dup }
+                    client.send RoomList.build(room_list)
                 when LeaveRoom
-                    leave_room message[:name], client
-                    # TODO Check for error returns and pass them as Error messages once Error messages are implemented.
+                    left, reason = leave_room message[:name], client
+                    if left == false
+                        # TODO Send error message.
+                        STDERR.puts reason
+                    end
                 when RequestRoomMemberList
-                    client.send room_members message[:name]
+                    client.send room_members(message[:name])
                 else
                     STDOUT.puts "[Debug] unrecognized message received:"
                     p message
@@ -254,17 +294,14 @@ module Chat
                 # Store the client socket before invoking a new thread to listen. This avoids a race condition
                 # between the two threads by ensuring that client is added to the socket list by the time
                 # the thread is spawned. After we have a thread object, we'll store that, too.
-                client = ConnectedClient.new
-                client.socket = socket
+                client = Client.new(socket)
                 thread = Thread.new do 
                     if greet(client)
                         listen(client)
                         clean_up(client)
                     end
                 end
-                @client_info_lock.synchronize do
-                    client.thread = thread
-                end
+                @client_info_lock.synchronize { client.thread = thread }
             end
         end
     end
